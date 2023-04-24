7814dcd35ee1358fcbf341c19da0cc07e78ba085
gin721 <sachin7o2o1o@gmail.com>
2023-04-25 01:56:31 +0530

    Bug Fixed
    TIMESTAMP : 25-04-2023 01:56 AM
    DESC : A small bug in the exception was fixed

--- Project/src/main/java/org/epharmacy/app/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package org.epharmacy.app;
-
-public class ResourceNotFoundException extends RuntimeException{
-	public static final long serialVersionUID = 1L;
-	
-	public ResourceNotFoundException(String message){
-		super(message);
-	}
-}
